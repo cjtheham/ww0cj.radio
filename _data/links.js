cdebfd5d5f668d710f26dff82e37153a4f52979c
cjtheham <cjtheham@protonmail.com>
2024-02-23 15:52:31 -0600

    misc changes

--- _data/links.js
+++ _data/links.js
@@ -3,5 +3,12 @@ module.exports = [
     "link": "https://dontasktoask.com/",
     "title": "Don't Ask to Ask",
     "description": "When you're in an online community and have a question over a topic, don't ask if you can ask the question and instead just ask the question and open the floor to anyone!",
+    "added": "22-Feb-2024"
   },
+  {
+    "link": "https://ragchew.app",
+    "title": "Ragchew.app",
+    "description": "A well-designed web interface for NetLogger that makes it easy for amateur radio operators to watch their favorite nets in real-time. I even wrote a blog post about it!",
+    "added": "23-Feb-2024"
+  }
 ];
